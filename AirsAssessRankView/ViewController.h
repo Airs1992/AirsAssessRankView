7a016a3fc66cdc936ab8a48ab8145ae9d41dd324
洪尘 <hongchen19921025@qq.com>
2016-05-19 14:48:32 +0800

    Initial Commit

---
+++ AirsAssessRankView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirsAssessRankView
+//
+//  Created by 洪尘 on 16/5/19.
+//  Copyright © 2016年 洪尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AirsAssessRankView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AirsAssessRankView
+//
+//  Created by 洪尘 on 16/5/19.
+//  Copyright © 2016年 洪尘. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
